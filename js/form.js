f448d4a90ca029e2c1a603f2aebcfef204ff1cc8
andresger <andres.gerz@gmail.com>
2019-01-24 20:05:37 -0300

    I learned forms with javascript and jquery

--- js/form.js
+++ js/form.js
@@ -1,37 +1,172 @@
-  
+var age;  
 
 function begin(){
 
-  var name=document.getElementById("name");
+  var age=document.getElementById("age");
 
-  var surname=document.getElementById("surname");
 
-  name.addEventListener("input",validation,false);
+  age.addEventListener("change",change_range,false);
 
-  surname.addEventListener("input",validation,false);
+  document.personal_data.addEventListener("invalid",validation,true);
 
-  validation();
+  document.getElementById("send").addEventListener("click",send,false);
 }
 
-function validation(){
-  if (name.value=="" && surname.value==""){
+function change_range(){
 
-    name.setCustomValidity("Introduce name or surname");
+  var out=document.getElementById("range");
 
-    name.style.background="#FFDDDD";
+  var calculate=age.value-40;
 
-    surname.style.background="#FFDDDD";
+  if (calculate<40){
+    calcuate=0;
+
+    age.value=40;
   }
-  else{
-    name.setCustomValidity("");
+  out.innerHTML=calculate+" a "+ age.value;
+}
 
-    name.style.background="#FFFFFF";
 
-    surname.style.background="#FFFFFF";
+function validation(e){
+  var element=e.target;
+  element.style.background="#FFDDDD";
+}
 
+function send(){
+  var correct=document.personal_data.checkValidity();
+
+  if (correct==true){
+    document.personal_data.submit();
   }
 
 }
 
 
 window.addEventListener("load",begin,false);
+
+// $(document).ready(function (e) {
+//   $("#send").click(confirm_data) 
+// });
+
+// function confirm_data(){
+
+//   // var myUser = $("#name").val();
+//   // alert("The user's name is: "+ myUser);
+//   $(":input").each(function () { 
+//     alert($(this).val());
+
+//   if ($("#male").prop("checked")){
+//     alert("You are male");
+//   }else{
+//     alert("You are female");
+//   }
+     
+//   });
+
+// }
+//==========================================================================================
+
+$(document).ready(function () {
+  $("#name").focus(function (){
+    
+    var fieldNAme = $(this);
+
+    if(fieldNAme.val()==fieldNAme.attr("value")){
+
+      fieldNAme.val("");
+    }
+  });
+
+  $("#form-datas").submit(function(){
+    
+    if($("#name").val()=="" || $("#name").val()=="Obligatorio nombre"){
+      alert("Complete to the name please: ");
+
+      $("#name").focus();
+
+      return false;
+    }
+  });
+
+  $("#age").blur(function () { 
+    var userAge=$(this).val();
+
+    if(isNaN(userAge)){
+      alert("Introduce a number please: ");
+
+      $(this).focus();
+      $(this).val("");
+
+    }
+    
+  });
+
+  var myArray=new Array();
+
+  var i=0;
+
+  $(":checkbox").click(function () { 
+
+    $("#knowledge").text("");
+
+    $(":checkbox").each(function () {
+      if ($(this).prop("checked")){
+        $("#knowledge").append($(this).val() + "<br/>" );
+      } 
+       
+    });
+    
+    // myArray[i]=$(this).val();
+    // // alert("Position: " + i + " " + myArray[i]);
+
+    // $("#knowledge").append(myArray[i]);
+    // i++;
+    
+  });
+
+  $("#refer").change(function() {
+    
+    alert("The country is: "+ $(this).val() );
+    
+  });
+});
+
+$(document).ready(function () {
+
+  // $("#paypal").click(function () {
+
+  //   $("#e-mail").attr("disabled",false).focus().css("background-color","white") ;
+  //   $("#target").attr("disabled", true).css("background-color","lightblue");
+
+  //   });
+  
+  // $("#visa, #mastercard").click(function() {
+    
+  //   $("#target").attr("disabled", false).focus().css("background-color","white");
+  //   $("#e-mail").attr("disabled", true).css("background-color","lightblue") ; 
+
+  // });
+
+  $("#visa, #mastercard").click(function () { 
+    
+    $("#target").css("visibility", "visible");
+    $("#nro-target").css("visibility", "visible");
+    
+    $("#mail").css("visibility", "hidden");
+    $("#e-mail").css("visibility", "hidden");
+    
+    $("#target").focus();
+  });
+
+  $("#paypal").click(function () { 
+    
+    $("#target").css("visibility", "hidden");
+    $("#nro-target").css("visibility", "hidden");
+    
+    $("#mail").css("visibility", "visible");
+    $("#e-mail").css("visibility", "visible");
+    
+    $("#e-mail").focus();
+  });
+  
+});
